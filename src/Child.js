21330e97f21354d45b1283b89cec111d6ffff87e
Adan Escamilla <adanescamilla580@gmail.com>
2021-03-16 16:39:01 -0700

    Done.

--- src/Child.js
+++ src/Child.js
@@ -1,15 +1,34 @@
+
 import React, { Component } from 'react'
 import { getRandomColor } from './randomColorGenerator.js'
 
 class Child extends Component {
   render() {
     return (
-      <div
+      <div onClick={() => this.props.handleColorChange(getRandomColor)}
         className="child"
-        style={{backgroundColor: "#FFF"}}
+        style={{backgroundColor: this.props.color}}
       ></div>
     )
   }
-}
+  //now we use this.props.color to use that initial state color
+  //that the parent claimed
 
+  //then we add in getRandomColor to our onClick 
+  //so now it has access the parent ChangeColor -> childrenColor: NewChildColor
+}
 export default Child
+//now with that function that we passed in we want to pass
+//it into a event handler so we do a 'onClick' event handler
+//and PASS IN THAT PROP FUNCTION, 
+//the event handler LISTENS FOR A CLICK 
+//ON THAT DIV/CHILD (NO BUTTON NEEDED)
+//so when WE click ON DIV/CHILDREN, that event 
+//calls the handleColorChange
+
+
+
+//NOW WE WANT TO CHANGE THE BACKGROUND COLOR AS WELL
+//data CAN ONLY GO UP AND DOWN CHILD / PARENT 
+
+
--- src/Parent.js
+++ src/Parent.js
@@ -3,22 +3,48 @@ import { getRandomColor } from './randomColorGenerator.js'
 import Child from './Child'
 
 class Parent extends Component {
-
+  
   constructor() {
     super()
     this.state = {
-      color: getRandomColor()
+      color: getRandomColor(),
+      childrenColor: '#FFF'
     }
   }
+//we set the initial state to getRandomColor which is a 
+//component that generates random colors.
+
+//NOW the initial color of children lives in the Parent 
+//like this.. (childrenColor: '#FFF')
+
+  changeColor = (newChildColor) => {
+    this.setState({
+      color: getRandomColor(),
+      childrenColor: newChildColor
+    })
+  }
+  //we also do a setState that set it to a randomColor AGAIN
+  
+  //we then update the changeColor, where it update childrenColor
+  //and sets that to newChildColor
 
   render() {
     return (
       <div className="parent" style={{backgroundColor: this.state.color}}>
-        <Child />
-        <Child />
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor}/>
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor}/>
       </div>
     )
   }
+//we pass a state changing function as a prop to both child components
+//as handleColorChange={this.changeColor}, now child will have a prop
+//that is handleColorChange, SAME FUNCTION AS PARENT 'changeColor'
+
+//SINCE the data that represents the children initial color
+//lives in the parent, we can PASS THAT ON AS PROPS TO THE CHILDS
+//like this... color={this.state.childrenColor}
+//now we can use that in the children 
 }
 
-export default Parent
+export default Parent;
+
